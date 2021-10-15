26c1144f9f5ae0bfc870f8e90b8ef15826529121
mp209id <92532754+mp209id@users.noreply.github.com>
2021-10-15 07:19:26 +0700

    asu

---
+++ kontolodon.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import sys
+import os
+
+
+def main():
+        os.system('clear')
+        os.system('figlet spam tai')
+        banner= '''
+
+        [+]Author:ACciL
+        [+]YouTube:mp209id
+        '''
+
+        print(banner)
+        no = input('target : ')
+        jum = input('jumlah spam : ')
+
+        head = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM->
+        "Referer": "https://www.mapclub.com/en/user/signup>
+        "Host": "cmsapi.mapclub.com",
+        }
